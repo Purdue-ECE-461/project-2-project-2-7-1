67ab91e210f653fafbb4b3626083f86b4ee4944c
AnthonyEshleman14 <53715103+AnthonyEshleman14@users.noreply.github.com>
2021-11-19 14:18:53 -0500

    removed buggy test, added functionality for cores

--- djangoapi/packageApp/packageApp/settings.py
+++ djangoapi/packageApp/packageApp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'packages',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'packageApp.urls'
@@ -149,3 +151,7 @@ REST_FRAMEWORK = {
        'rest_framework.authentication.TokenAuthentication', ),
    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser', ),
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
